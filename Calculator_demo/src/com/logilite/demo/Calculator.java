57a0badb45e375d975bed137c956ef9783648011
dhruv7100 <dmunjani2@gmail.com>
2021-09-17 18:00:26 +0530

    testing work for conflicts

--- Calculator_demo/src/com/logilite/demo/Calculator.java
+++ Calculator_demo/src/com/logilite/demo/Calculator.java
@@ -43,14 +43,9 @@ public class Calculator {
 	        System.out.println(number1 + " * " + number2 + " = " + result);
 	        break;
 
-	      // performs division between numbers
-	      case '/':
-	        result = number1 / number2;
-	        System.out.println(number1 + " / " + number2 + " = " + result);
-	        break;
 
 	      default:
-	        System.out.println("invalid operator");
+	        System.out.println("tesing work");
 	        break;
 	    }
 
